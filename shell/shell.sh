fee0f38db7609ecb5955b6c23f447c3d61f0d261
Tania Cao <tcaosu@stuy.edu>
2019-03-14 07:14:55 -0400

    add shell

---
+++ shell/shell.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ls # print a list of items in current directory
+
+echo Hello World # shell automatically places a space in between each parameter being echoed
+
+echo "What is your name?"
+read name
+echo "How are you, $name?"
+read remark
+echo "I am $remark too!"
+
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo `hello world
+echo 'hello' world
